085036663708b5bcedfb49bb92eb866fe0369635
John Peach <githubspam@jppeach.com>
2017-07-02 17:52:38 -0700

    dev-23 cleaned up read_driver doc

--- R/driver.R
+++ R/driver.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1df6bbebe6c971e40213b05dd97ec51b4a60eee54a59f61a5a3a55048001cbde
-size 1479
+oid sha256:219d0493f7c1d62c13aebc97b051933f1430457f0419f100ac44f74fa02d9e10
+size 1427
--- R/read_driver.R
+++ R/read_driver.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d12057c54b785e634b1823bcbf4d6a428c2c61b8dc2a648531e721bb359cce6e
-size 3340
+oid sha256:6c4ddb8b28b42be3de3985611ebe1f51c63e2386113069539ab5394db4a51efb
+size 3625
