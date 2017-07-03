cdeed12ce615e841162485d954e197b47b2bce97
John Peach <githubspam@jppeach.com>
2017-07-02 17:58:26 -0700

    dev-23 fixed warning about dplyr and magrittr using %>%

--- R/recordingStitch.R
+++ R/recordingStitch.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f53063656cd159f33a3f02dc17c68c18ee04bc042d6c998301cf9dd2ca12e858
-size 3406
+oid sha256:3fddc11858f3bc81cc3945ef3baf527cbe77134a5cc439f471f308d1c52bbbb2
+size 3427
