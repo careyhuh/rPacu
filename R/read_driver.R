561a7e13b0e409a8818029fef18ab0509cd1ed21
John Peach <githubspam@jppeach.com>
2017-07-03 16:38:57 -0700

    dev-23 added tests for read_driver and fixed empty df issue

--- R/read_driver.R
+++ R/read_driver.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:543741ad865257137df81afe1f3bbd0abfedb9359de5e35b6305e23154bbd992
-size 3731
+oid sha256:2acd2f4dbc0cf17dc048c779b4c50186365425fc457db70414ec8b60d31b3d93
+size 4552
--- tests/testthat/test-read_driver.R
+++ tests/testthat/test-read_driver.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13a718eb9cc3e462ea3135f8646362b304c36670e13b6b44d84c2693bb33c299
-size 1476
+oid sha256:d3736526808884b99c1a9416c8ae68752f02afcd4670091acad89a3900faedde
+size 3064
