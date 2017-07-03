a3f79378349bb71ab62889c74dbd87a86bca7691
Diyue Gu <diyueg@uci.edu>
2017-07-03 16:19:54 -0700

    dev-23 documentation unsolved

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80fceba0769bbeda6ccf0b11924e01a5dc4b3a46647881c07e9242bdd0225a26
-size 56
+oid sha256:af74e22eaea19d2e6279a319238352c299e6e2fd00c79df8451fb1dc49dec8c5
+size 53
