078557598787e7352a4457d9e7341af890aee9f2
Diyue Gu <diyueg@uci.edu>
2017-07-03 12:50:27 -0700

    dev-23 all tests

--- tests/testthat/test-read_driver.R
+++ tests/testthat/test-read_driver.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d3c03e9e2ff536af2dfbe084dabc90b558f2719d5bd6aa210083125966558c5
-size 1206
+oid sha256:13a718eb9cc3e462ea3135f8646362b304c36670e13b6b44d84c2693bb33c299
+size 1476
