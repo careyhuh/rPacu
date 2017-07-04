49cbc1c71ffcca938f40deca67596f102c5dcc45
Diyue Gu <diyueg@uci.edu>
2017-07-03 19:12:02 -0700

    dev-23 clear up the merge conflict again

--- tests/testthat/test-read_driver.R
+++ tests/testthat/test-read_driver.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94bfba60d778525e6a6142076ccce242b4ffa2cd2ce23fb9317849f9803071ea
-size 3116
+oid sha256:b4c33ff7947161bf6e70e3a1ee4af1ef99db1a99ac665148eb74f55ece7c32e4
+size 3045
