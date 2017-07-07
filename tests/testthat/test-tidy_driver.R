9d2d1e972e7c71c349f67e3403fe94afbf94ebc0
Diyue Gu <diyueg@uci.edu>
2017-07-07 16:46:20 -0700

    dev-34 tests updated

--- tests/testthat/test-tidy_driver.R
+++ tests/testthat/test-tidy_driver.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca08271db58f4a2c83d99ee988dd2de74450a0af52ffd97f609005d7c8000194
-size 577
+oid sha256:45119cfabc3dea1c1cbdc88f094e014e7173cb481eabf81dcd09a411bd3ea87e
+size 963
