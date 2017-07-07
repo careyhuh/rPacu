f8db22fa3e159b556f099e828dd75bf5b062bad7
Diyue Gu <diyueg@uci.edu>
2017-07-07 14:46:34 -0700

    dev-34 achieve function

--- R/tidy_driver.R
+++ R/tidy_driver.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a601f2c3d3e4e55267304fcec5c0f9441244ced71d0bfb84e325e2837e419caa
-size 1527
+oid sha256:d86eaa9acec49464f0dd8ed24ef75391739ef4170d9a2a6e80229cec98d3ed09
+size 1141
