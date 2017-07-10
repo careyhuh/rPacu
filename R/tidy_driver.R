b846f0f95273781646fa3dc3d7e14664d4349759
Diyue Gu <diyueg@uci.edu>
2017-07-10 14:55:51 -0700

    dev-34 R CMD check passing with 0/0/1 (run the example)

--- R/tidy_driver.R
+++ R/tidy_driver.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75616366c5611d43dad4c337f6451fbff0e3aa34660057e6a496a81d9b644f52
-size 1149
+oid sha256:25445a208fcdea8ee86f4ee78b4f3b4cbf28a12d945f1208ddd7b516ebadbcf6
+size 1145
