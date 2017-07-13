866d60a4c3dfd37ad36af278458770343bb43f1d
Diyue Gu <diyueg@uci.edu>
2017-07-13 11:29:48 -0700

    dev-39 avoid duplicates

--- R/tidy_excel.R
+++ R/tidy_excel.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f12564dee7e4d74c9a9d5656d24a2271c6a82260b72fa31603998d3e5fcc15a
-size 2669
+oid sha256:c605d0687ec63d6b2f9fc69d40b85c693f2886288ffa405ced0bad7054db86dc
+size 2449
