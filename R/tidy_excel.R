5228376e511471cd42daebb9e8d4bd1a61ac4fd8
Diyue Gu <diyueg@uci.edu>
2017-07-16 15:37:17 -0700

    dev-39

--- R/tidy_excel.R
+++ R/tidy_excel.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd4006fcd1af1dbf9c60d297d3bcd8059573417c9c0034cfcb55261670cb51de
-size 2860
+oid sha256:b6d84a86e634eb8be3dc507dc992a21b89e88829f3e5815fe2fe90a6caeda67d
+size 2965
