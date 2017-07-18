4fe0a7b471965223005a259651a242bd5f7d4674
John Peach <githubspam@jppeach.com>
2017-07-17 22:05:29 -0700

    Fixed documentation error - check 0/0/0

--- R/get_pacu_table.R
+++ R/get_pacu_table.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a944ecc259fe6d5ca84549b6c43aa6d3206650759a28bf36379f42be3d63c03a
-size 3102
+oid sha256:34cdfb87470800fa240f39eef0cc9e581b2de628b28a0e1f282ff2f6a348cb45
+size 3110
