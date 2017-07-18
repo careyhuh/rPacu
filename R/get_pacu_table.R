f3a81e4694715bd30408172613b77107ce27b75c
John Peach <githubspam@jppeach.com>
2017-07-17 18:54:33 -0700

    Fixed bug in example

--- R/get_pacu_table.R
+++ R/get_pacu_table.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:446638a7f59db54ce0b6a2046c1761416ca31d9531b6279e29e530cbe4b85659
-size 3060
+oid sha256:a944ecc259fe6d5ca84549b6c43aa6d3206650759a28bf36379f42be3d63c03a
+size 3102
