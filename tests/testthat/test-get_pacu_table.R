fa398ab8d16e1d6f6b24603b433d78da478bb89e
Diyue Gu <diyueg@uci.edu>
2017-07-28 17:08:44 -0700

    dev-45

--- tests/testthat/test-get_pacu_table.R
+++ tests/testthat/test-get_pacu_table.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ea603e5edf49fc8baa1751463f557174e765b288a32f9dbeae4e33498838e7c
+oid sha256:c9acddfefdbd10c01114e02e5e8dd02027a0a4964d6adb1883fac236e3df9f2b
 size 2015
