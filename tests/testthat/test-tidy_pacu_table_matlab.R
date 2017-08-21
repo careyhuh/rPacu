3ef14934db3647ebba969d6b0e458b44af442b18
nzhu <zny_ok1989@hotmail.com>
2017-08-21 14:54:17 -0700

    modified test-tidy_pacu_table_matlab.R

--- tests/testthat/test-tidy_pacu_table_matlab.R
+++ tests/testthat/test-tidy_pacu_table_matlab.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:537fade102f64885f50e35ca5b61e2c61ca92c7a335be755ef302f738a1bdc62
-size 606
+oid sha256:f01c7b4e33a14be5861c3faad656ef66eeb319a6afd8c3b3a36f54ef5677d314
+size 629
