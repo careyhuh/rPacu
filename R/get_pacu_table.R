f827e95b83b2b690f803b0ee0879850f290b3a95
nzhu <zny_ok1989@hotmail.com>
2017-08-28 14:14:03 -0700

    changed code

--- R/get_pacu_table.R
+++ R/get_pacu_table.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f77928c6141125eb2892e61ca88b6f3c2d602548fee7ed0606fedc50e2922b5
-size 4537
+oid sha256:9524706390d4faaefa73c1ed105d22730dbad7f00b385d269a48377acb206105
+size 4530
