844a44510c03403cd318430e52e3eeb9123ea253
John Peach <githubspam@jppeach.com>
2018-09-28 22:48:07 -0700

    No filtering with p_cutoff = NA

--- R/sf_peak.R
+++ R/sf_peak.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2f4a3f14e3f64ad1f2b225b3283f680839fca797f0e93a3355b0134931cad28
-size 2930
+oid sha256:32225bb5725845cf2b3665d9e96b6ea3aec545ad638b951b4b101de6369788bc
+size 2975
